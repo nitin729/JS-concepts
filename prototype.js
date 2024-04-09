ed9308a0dd36511722361366a6c7520e29de78d1
nitin729 <nitin117@tuta.io>
2024-04-09 07:28:11 +0530

    prototype in js

---
+++ prototype.js
@@ -0,0 +1,9 @@
+function multiplyByFive(num) {
+  return num * 5;
+}
+multiplyByFive.power = 2;
+console.log(multiplyByFive(5));
+console.log(multiplyByFive.power);
+console.log(multiplyByFive.prototype);
+
+function createUser(username, score) {}
